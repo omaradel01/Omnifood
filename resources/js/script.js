dab4d26f944b19c9d3027ce7044f26132dceec5d
Omar Adel <omaradelezz54@gmail.com>
2022-10-03 12:59:56 +0200

    Added some animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -64,7 +64,7 @@ $(function () {
   /* animations on scroll */
   $('.js--wp-1').waypoint(
     function (direction) {
-      $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+      $('.js--wp-1').addClass('animate__animated animate__fadeInUp');
     },
     {
       offset: '50%',
@@ -91,7 +91,25 @@ $(function () {
 
   $('.js--wp-4').waypoint(
     function (direction) {
-      $('.js--wp-4').addClass('animate__animated animate__bounce');
+      $('.js--wp-4').addClass('animate__animated animate__backInLeft');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-5').waypoint(
+    function (direction) {
+      $('.js--wp-5').addClass('animate__animated animate__backInLeft');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-6').waypoint(
+    function (direction) {
+      $('.js--wp-6').addClass('animate__animated animate__backInLeft');
     },
     {
       offset: '50%',
